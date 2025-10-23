46a50be82756869796e588f2bbf2a86a2af95a43
romastokriceri <kritskiyroma896@gmail.com>
2025-10-24 00:43:35 +0300

    Host version of auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -51,12 +51,12 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
+        user_id: int = payload.get("sub")  # Очікуємо ID, а не username
+        if user_id is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
-    user = db.query(models.User).filter(models.User.username == username).first()
+    user = db.query(models.User).filter(models.User.id == user_id).first()  # Шукаємо за ID
     if user is None:
         raise credentials_exception
     return user
